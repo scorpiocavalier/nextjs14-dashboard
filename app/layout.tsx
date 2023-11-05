6095bf773c34f65b980a9431762125df3c91a925
scorpiocavalier <art.can.fly@gmail.com>
2023-11-05 18:55:37 -0500

    add 2 google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
-import '@/app/ui/global.css';
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/font";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
-  );
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
